c807beeaa456bdb04d908573e5625dbed46433b2
Tamer Saadeh <tamersaadeh@gmail.com>
2012-03-03 22:28:02 +0100

    added the common.sh into the scripts directory so I can modify the parts I don't need and to make it simpler, idlely I would like to get rid of it entirely!

--- scripts/use_common.sh
+++ scripts/use_common.sh
@@ -1,23 +1,23 @@
 # 'source' my common functions
-if [ -r /tmp/common.sh ]; then
-    source /tmp/common.sh
-    if [ $? -ne 0 ]; then
-        echo "ERROR! Couldn't import common functions from /tmp/common.sh"
-        rm /tmp/common.sh 2>/dev/null
-        exit 1
-    else
-        source /tmp/common.sh
-        update_thyself        
-    fi
-else
-    echo "Downloading common.sh"
-    wget --no-check-certificate -q "https://github.com/flexiondotorg/common/raw/master/common.sh" -O /tmp/common.sh
-    chmod 666 /tmp/common.sh
-    source /tmp/common.sh
-    if [ $? -ne 0 ]; then
-        echo "ERROR! Couldn't import common functions from /tmp/common.sh"
-        rm /tmp/common.sh 2>/dev/null
-        exit 1
-    fi
-fi
-
+#if [ -r /tmp/common.sh ]; then
+#    source /tmp/common.sh
+#    if [ $? -ne 0 ]; then
+#        echo "ERROR! Couldn't import common functions from /tmp/common.sh"
+#        rm /tmp/common.sh 2>/dev/null
+#        exit 1
+#    else
+#        source /tmp/common.sh
+#        update_thyself        
+#    fi
+#else
+#    echo "Downloading common.sh"
+#    wget --no-check-certificate -q "https://github.com/flexiondotorg/common/raw/master/common.sh" -O /tmp/common.sh
+#    chmod 666 /tmp/common.sh
+#    source /tmp/common.sh
+#    if [ $? -ne 0 ]; then
+#        echo "ERROR! Couldn't import common functions from /tmp/common.sh"
+#        rm /tmp/common.sh 2>/dev/null
+#        exit 1
+#    fi
+#fi
+cp ./$SCRIPTS/common.sh /tmp
