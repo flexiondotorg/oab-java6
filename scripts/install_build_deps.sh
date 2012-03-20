011711108a656fe74c8c594eb24064948a84b7fd
Tamer Saadeh <tamersaadeh@gmail.com>
2012-03-20 10:27:43 +0100

    Removed the work around for precise, since it was fixed.
    See:
    - https://github.com/rraptorr/sun-java6/issues/5
    - https://bugs.launchpad.net/ubuntu/+source/ia32-libs/+bug/947495

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -8,13 +8,6 @@ if [ "${LSB_ARCH}" == "amd64" ]; then
     BUILD_DEPS="${BUILD_DEPS} lib32asound2 ia32-libs"
 fi
 
-# Workaround until both of these are fixed:
-# - https://github.com/rraptorr/sun-java6/issues/5
-# - https://bugs.launchpad.net/ubuntu/+source/ia32-libs/+bug/947495
-if [ "${LSB_CODE}" == "precise" ]; then
-    BUILD_DEPS="${BUILD_DEPS}  libxtst6:i386"
-fi
-
 # Install the Java build requirements
 ncecho " [x] Installing Java build requirements "
 apt-get install -y --no-install-recommends ${BUILD_DEPS} >> "$LOG" 2>&1 &
